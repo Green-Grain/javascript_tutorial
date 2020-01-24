6440bfe22a4bebe1d2103457b1fbef53f2adafb9
Green-Grain <zwei.blattes@gmail.com>
2020-01-24 19:38:19 +0900

    5.2 タスク

--- 05.02/src/script.js
+++ 05.02/src/script.js
@@ -29,6 +29,16 @@ function random(min, max) {
   // }
   return min + Math.random() * (max - min);
 };
-alert(random(1, 5));
-alert(random(1, 5));
-alert(random(1, 5));
+// alert(random(1, 5));
+// alert(random(1, 5));
+// alert(random(1, 5));
+
+function randomInteger(min, max) {
+  // return (Math.random() * 10).toFixed(0) % (max - min) + 1;
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+// alert(randomInteger(1, 5));
+// alert(randomInteger(1, 5));
+// alert(randomInteger(1, 5));
+
