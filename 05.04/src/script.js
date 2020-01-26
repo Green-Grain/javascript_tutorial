580990a59b4318631b1ef7f7c155fcbd0b9f4d6a
Green-Grain <zwei.blattes@gmail.com>
2020-01-26 14:35:54 +0900

    5.4 タスク

--- 05.04/src/script.js
+++ 05.04/src/script.js
@@ -27,5 +27,28 @@ function sumInput() {
   }
   return total;
 };
-console.log(sumInput());
+// console.log(sumInput());
+
+
+function getMaxSubSum(arr) {
+  let maxSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let sumFixedStart = 0;
+    for (let j = i; j < arr.length; j++) {
+      sumFixedStart += arr[j];
+      maxSum = Math.max(maxSum, sumFixedStart);
+    }
+  }
+
+  return maxSum;
+}
+function task5() {
+  alert( getMaxSubSum([-1, 2, 3, -9]) );
+  alert( getMaxSubSum([-1, 2, 3, -9, 11]) );
+  alert( getMaxSubSum([-2, -1, 1, 2]) );
+  alert( getMaxSubSum([1, 2, 3]) );
+  alert( getMaxSubSum([100, -9, 2, -3, 5]) );
+}
+task5();
 
