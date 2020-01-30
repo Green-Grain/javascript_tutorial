20bd54e70206bf46fbe60c41039fc8814ae338a0
Green-Grain <zwei.blattes@gmail.com>
2020-01-30 18:22:21 +0900

    05.12 タスク

--- 05.12/src/script.js
+++ 05.12/src/script.js
@@ -10,5 +10,24 @@ function task1() {
 }
 // task1();
 
+function task2() {
+  let room = {
+      number: 23
+  };
 
+  let meetup = {
+    title: "Conference",
+    occupiedBy: [{name: "John"}, {name: "Alice"}],
+    place: room
+  };
 
+  // 循環参照
+  room.occupiedBy = meetup;
+  meetup.self = meetup;
+
+  alert( JSON.stringify(meetup, function replacer(key, value) {
+    if (key != "" && value == meetup)  return undefined;
+    return value;
+  }));
+}
+task2();
